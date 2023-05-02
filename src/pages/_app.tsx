8ebd1e209baf887027b97e4341a0998ef835bf26
Nam Hoang Nguyen <nguyenbydoing@gmail.com>
2023-05-02 14:11:28 -0700

    Add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import { ClerkProvider } from '@clerk/nextjs';
+import { type AppType } from 'next/dist/shared/lib/utils';
 
-import "~/styles/globals.css";
+import '~/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<ClerkProvider {...pageProps}>
+			<Component {...pageProps} />
+		</ClerkProvider>
+	);
 };
 
 export default MyApp;
